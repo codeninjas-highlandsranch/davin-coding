7b49f9ab9466563813efd2e56465e14b497b403d
Amanda Tree <amandasimonds9@gmail.com>
2020-08-28 16:35:07 -0600

    script

--- script.js
+++
@@ -1,6 +0,0 @@
-var canvas = document.getElementById("canvas")
-var ctx = canvas.getContext("2d");
-
-//draw a rectangle on the canvas
-ctx.fillStyle = "green";
-ctx.fillRect(0, 0, 300, 200) //(x position, y position, width, height)
