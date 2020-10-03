4bf6f2118044e9a5dfa57e3ccf96ae284bdff4e0
unknown <codesenseiamanda@gmail.com>
2020-10-02 18:45:42 -0600

    game character

--- Game/game.js
+++ Game/game.js
@@ -1,8 +1,9 @@
 function startGame() {
     console.log("start game")
     myGameArea.start();
-    myGamePiece = new component(30, 30, "red", 10, 120);
-  }
+    myGamePiece = new component(30, 30,"../slimy_boi.png", 10, 10, "image");
+    myGamePiece.update()
+    }
   
   var myGameArea = {
     canvas : document.querySelector(".game-canvas"),
@@ -13,14 +14,35 @@ function startGame() {
     }
   }
 
-  function component(width, height, color, x, y) {
+  function component(width, height, color, x, y, type) {
+  this.type = type;
+  if(type == "image" ) {
+    this.image = new Image();
+    this.image.src = color;
+  }
     this.width = width;
     this.height = height;
+    this.speedX = 0;
+    this.speedY = 0;
     this.x = x;
     this.y = y;
+    this.update = function () {
+      console.log("game piece update")
     ctx = myGameArea.context;
-    ctx.fillStyle = color;
-    ctx.fillRect(this.x, this.y, this.width, this.height);
+      if(type == "image" ) {
+        ctx.drawImage(this.image,
+        this.x,
+        this.y,
+        this.width, this.height);
+      } else {
+        ctx.fillStyle = color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);     
+    } 
   }
+}
+
+function updateGame(){
+  myGamePiece.update();
+}
 
   startGame();
